ea63927f2af8dbe12a087bcea08682a19856934d
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2018-04-14 15:42:12 +0200

    Adjust sample grid

--- src/components/ExampleGrid.js
+++ src/components/ExampleGrid.js
@@ -9,8 +9,14 @@ class ExampleGrid extends Component {
   state = {
     layout: [
       { ...gridConfig, i: 'a', x: 0, y: 0, w: 4, h: 4 },
-      { ...gridConfig, i: 'b', x: 6, y: 0, w: 6, h: 4 },
-      { ...gridConfig, i: 'c', x: 0, y: 6, w: 12, h: 4 }
+      { ...gridConfig, i: 'b', x: 4, y: 0, w: 4, h: 4 },
+      { ...gridConfig, i: 'c', x: 8, y: 0, w: 4, h: 4 },
+      { ...gridConfig, i: 'd', x: 0, y: 4, w: 4, h: 4 },
+      { ...gridConfig, i: 'e', x: 4, y: 4, w: 4, h: 4 },
+      { ...gridConfig, i: 'f', x: 8, y: 4, w: 4, h: 4 },
+      { ...gridConfig, i: 'g', x: 0, y: 8, w: 4, h: 4 },
+      { ...gridConfig, i: 'h', x: 4, y: 8, w: 4, h: 4 },
+      { ...gridConfig, i: 'i', x: 8, y: 8, w: 4, h: 4 }
     ]
   };
 
@@ -18,7 +24,7 @@ class ExampleGrid extends Component {
     const { layout } = this.state;
 
     return (
-      <GridLayout layout={layout} cols={12} rowHeight={50} width={1200}>
+      <GridLayout layout={layout} cols={12} rowHeight={50} width={1300}>
         {layout.map(item => (
           <div key={item.i}>
             <ExampleChart key={item.i} />
