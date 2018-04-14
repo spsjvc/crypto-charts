73892ea896d1cbb2efe132a7695725d0fb28c3b4
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2018-04-14 15:45:49 +0200

    Add Ant Design

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 
 import App from './components/App';
 
+import 'antd/dist/antd.css';
 import 'react-grid-layout/css/styles.css';
 import 'react-resizable/css/styles.css';
 
